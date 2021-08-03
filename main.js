d7d009f0cb00fdf83a7860026266d4cbb77ac0a7
Noah Ehrman <giantnoah@gmail.com>
2021-08-03 11:52:26 -0400

    "Feature Complete" Just need to make it cleaner and prettier

--- main.js
+++ main.js
@@ -5,8 +5,24 @@ function addCustomer() {
   document.getElementById('total').value = "";
   total = formatMoneyToInt(total);
   splitters.push([name,total]);
+  updateFeePercentages();
   updateUsers();
-  console.log(splitters);
+}
+
+function updateFees() {
+  fees = formatMoneyToInt(document.getElementById('fees').value);
+  tip = formatMoneyToInt(document.getElementById('tip').value);
+  updateFeePercentages();
+}
+
+function updateFeePercentages() {
+  total = 0;
+  for (let i = 0; i < splitters.length; i++) {
+    total += splitters[i][1];
+  }
+  tipPercent = tip / total;
+  feePercent = fees/ total;
+  updateUsers();
 }
 
 function formatMoneyToInt(input) {
@@ -22,7 +38,6 @@ function formatMoneyToInt(input) {
 }
 
 function formatMoneyToString(input) {
-  console.log(input);
   output = ".";
   output = "$" + Math.floor(input / 100) + output + (String(input % 100)+"00").slice(0,2);
   return output;
@@ -33,16 +48,34 @@ function updateUsers() {
   while(customerList.lastChild){
     customerList.removeChild(customerList.lastChild);
   }
+  // TODO: automate adding sections to container
   for (let i = 0; i < splitters.length; i++){
     var container = document.createElement("div");
     var name = document.createElement("h1");
+    var subtotal = document.createElement("p");
     var total = document.createElement("p");
+    var tip = document.createElement("p");
+    var fees = document.createElement("p");
+    var subtotalAmt = splitters[i][1];
+    var feeAmt = splitters[i][1]*feePercent;
+    var tipAmt = splitters[i][1]*tipPercent;
+    var totalAmt = subtotalAmt+feeAmt+tipAmt;
     name.appendChild(document.createTextNode(splitters[i][0]));
-    total.appendChild(document.createTextNode(formatMoneyToString(splitters[i][1])));
+    subtotal.appendChild(document.createTextNode(formatMoneyToString(subtotalAmt)));
+    tip.appendChild(document.createTextNode(formatMoneyToString(tipAmt)))
+    fees.appendChild(document.createTextNode(formatMoneyToString(feeAmt)))
+    total.appendChild(document.createTextNode(formatMoneyToString(totalAmt)))
     container.appendChild(name);
+    container.appendChild(subtotal);
+    container.appendChild(tip);
+    container.appendChild(fees);
     container.appendChild(total);
     customerList.appendChild(container);
   }
 }
 
 var splitters = new Array();
+var fees = 0;
+var tip = 0;
+var feePercent = 0.0;
+var tipPercent = 0.0;
